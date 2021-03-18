de7d3e5b38dd5edeee66650bdf42e5760339e61d
AndjelkaDj <andjelka.adj@elfak.rs>
2021-03-18 01:19:04 +0100

    Add project files.

---
+++ RentALLMongo/Form1.cs
@@ -0,0 +1,32 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RentALLMongo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //var connectionString = ConfigurationManager.AppSettings["mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false"];
+
+            var client = new MongoClient("mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false");
+            var database = client.GetDatabase("RentALLDb");
+
+            //var collection = database.GetCollection<User>("Users");
+
+        }
+    }
+}
